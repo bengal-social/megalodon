db16dde07370302dd6da206d6565a6fed2f4e7da
sk <sk22@mailbox.org>
2022-12-10 17:55:29 +0100

    fix wrong visibility in reply
    fix #140

--- mastodon/src/main/java/org/joinmastodon/android/fragments/ComposeFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/ComposeFragment.java
@@ -493,14 +493,14 @@ public class ComposeFragment extends MastodonToolbarFragment implements OnBackPr
 		spoilerEdit.addTextChangedListener(new SimpleTextWatcher(e->updateCharCounter()));
 		if(replyTo!=null){
 			replyText.setText(getString(R.string.in_reply_to, replyTo.account.displayName));
-			int visibilityNameRes = switch (statusVisibility) {
+			int visibilityNameRes = switch (replyTo.visibility) {
 				case PUBLIC -> R.string.visibility_public;
 				case UNLISTED -> R.string.visibility_unlisted;
 				case PRIVATE -> R.string.visibility_followers_only;
 				case DIRECT -> R.string.visibility_private;
 			};
 			replyText.setContentDescription(getString(R.string.in_reply_to, replyTo.account.displayName) + ". " + getString(R.string.post_visibility) + ": " + getString(visibilityNameRes));
-			Drawable visibilityIcon = getActivity().getDrawable(switch(statusVisibility){
+			Drawable visibilityIcon = getActivity().getDrawable(switch(replyTo.visibility){
 				case PUBLIC -> R.drawable.ic_fluent_earth_20_regular;
 				case UNLISTED -> R.drawable.ic_fluent_people_community_20_regular;
 				case PRIVATE -> R.drawable.ic_fluent_people_checkmark_20_regular;
