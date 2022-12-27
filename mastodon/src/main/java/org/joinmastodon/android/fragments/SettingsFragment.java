671338c16aa6d612161e6499e75405caa3bf4498
sk <sk22@mailbox.org>
2022-12-27 21:41:08 +0100

    different icon for delete notifs setting

--- mastodon/src/main/java/org/joinmastodon/android/fragments/SettingsFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/SettingsFragment.java
@@ -184,7 +184,7 @@ public class SettingsFragment extends MastodonToolbarFragment{
 			GlobalUserPreferences.save();
 			needAppRestart=true;
 		}));
-		items.add(new SwitchItem(R.string.sk_enable_delete_notifications, R.drawable.ic_fluent_delete_24_regular, GlobalUserPreferences.enableDeleteNotifications, i->{
+		items.add(new SwitchItem(R.string.sk_enable_delete_notifications, R.drawable.ic_fluent_mail_inbox_dismiss_24_regular, GlobalUserPreferences.enableDeleteNotifications, i->{
 			GlobalUserPreferences.enableDeleteNotifications=i.checked;
 			GlobalUserPreferences.save();
 			needAppRestart=true;
