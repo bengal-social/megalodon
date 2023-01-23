dab0c560e9bf4aa68debb234a5be0fc93ebe8041
sk <sk22@mailbox.org>
2023-01-23 12:23:15 +0100

    fix crash when recycler view is null

--- mastodon/src/main/java/org/joinmastodon/android/fragments/IsOnTop.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/IsOnTop.java
@@ -1,11 +1,13 @@
 package org.joinmastodon.android.fragments;
 
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 public interface IsOnTop {
     boolean isOnTop();
 
-    default boolean isRecyclerViewOnTop(RecyclerView list) {
+    default boolean isRecyclerViewOnTop(@Nullable RecyclerView list) {
+        if (list == null) return true;
         return !list.canScrollVertically(-1);
     }
 }
